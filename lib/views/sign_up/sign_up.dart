50fba0fde679147d00b425698d4cf0b2dbef7918
amrmahmoud333 <3mrma7moud69@gmail.com>
2022-02-09 14:48:08 +0200

    draw top cloud

--- lib/views/sign_up/sign_up.dart
+++ lib/views/sign_up/sign_up.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rafiq/views/painter/top_cloud.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double height(double n) {
+      return MediaQuery.of(context).size.height * (n / 851);
+    }
+
+    double width(double n) {
+      return MediaQuery.of(context).size.width * (n / 393);
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [TopCloud()],
+        ),
+      ),
+    );
   }
 }
