e24fc7e1d9d79b853b5e55a856e472c99aaf288b
mustafa-mahmoud203 <must.afa000262@gmail.com>
2022-02-09 17:37:04 +0200

    add input Field && add log button

---
+++ lib/style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+TextStyle lableInputField() {
+  return const TextStyle(
+      fontSize: 18,
+      //fontWeight: FontWeight.bold,
+      color: Color(0xFF6E75A0));
+}
