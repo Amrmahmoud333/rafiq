f7b83cf776a15c54c6372cb7182000afa5ccf574
amrmahmoud333 <3mrma7moud69@gmail.com>
2022-03-17 01:07:34 +0200

    add custom dot to app bar

--- lib/views/profile/widgets/profile_home.dart
+++ lib/views/profile/widgets/profile_home.dart
@@ -17,15 +17,17 @@ class ProfileHome extends StatelessWidget {
 
     return Expanded(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           SizedBox(
             height: h(33),
             width: w(373),
             child: AppBar(
-              elevation: 0,
+              elevation: 2,
               backgroundColor: const Color(0xffF7F4F8),
               bottom: TabBar(
+                indicator: CircleTabIndicator(
+                    color: const Color(0xff5B618A), radius: 3),
                 labelColor: const Color(0xff5B618A),
                 unselectedLabelColor: const Color(0xff5B618A).withOpacity(0.35),
                 labelStyle: Theme.of(context).textTheme.headline6,
@@ -80,5 +82,28 @@ class ProfileHome extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final BoxPainter _painter;
+  CircleTabIndicator({@required Color? color, @required double? radius})
+      : _painter = _CirclePainter(color!, radius!);
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) => _painter;
+}
+
+class _CirclePainter extends BoxPainter {
+  final Paint _paint;
+  final double radius;
+  _CirclePainter(Color color, this.radius)
+      : _paint = Paint()
+          ..color = color
+          ..isAntiAlias = true;
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration cfg) {
+    final Offset circleOffset =
+        offset + Offset(cfg.size!.width / 2, cfg.size!.height - radius);
+    canvas.drawCircle(circleOffset, radius, _paint);
   }
 }
