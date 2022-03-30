4fe8d143f9a81706abd3a47d48dd5cb3dbeae298
amrmahmoud333 <3mrma7moud69@gmail.com>
2022-03-30 19:25:23 +0200

    add style to map

--- lib/views/profile/widgets/tap_bar_view_widgets/map.dart
+++ lib/views/profile/widgets/tap_bar_view_widgets/map.dart
@@ -15,9 +15,41 @@ class Maps extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GoogleMap(
-        initialCameraPosition: CameraPosition(
-      target: LatLng(37.43296265331129, -122.08832357078792),
-    ));
+    double h(double n) {
+      return MediaQuery.of(context).size.height * (n / 851);
+    }
+
+    double w(double n) {
+      return MediaQuery.of(context).size.height * (n / 393);
+    }
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: h(30)),
+            child: Container(
+              width: w(150),
+              height: h(212.6),
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xff5B618A),
+                    spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: Offset(7, 5), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: const GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(37.43296265331129, -122.08832357078792),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
