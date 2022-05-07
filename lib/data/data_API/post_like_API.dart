1878907b28335e707719b7e39f48c5a7c6d9150f
amrmahmoud333 <3mrma7moud69@gmail.com>
2022-05-07 17:18:38 +0200

    add unlike to post like api

--- lib/data/data_API/post_like_API.dart
+++ lib/data/data_API/post_like_API.dart
@@ -3,7 +3,8 @@ import 'package:flutter/widgets.dart';
 import 'package:rafiq/core/constants/authentication_const.dart';
 import 'package:rafiq/core/constants/url.dart';
 import 'package:rafiq/data/data_API/dio_helper.dart';
-import 'package:rafiq/data/models/post_like.dart';
+import 'package:rafiq/data/models/post/is_like_model.dart';
+import 'package:rafiq/data/models/post/post_like.dart';
 
 class PostLikeAPI {
   Future<PostLikeModel> makeLikeToPost({required String postId}) async {
@@ -12,5 +13,22 @@ class PostLikeAPI {
       header: {'access-token': token},
     );
     return PostLikeModel.fromJson(response.data);
+  }
+
+  Future<IsLikeModel> isLike({required String postId}) async {
+    final response = await DioHelper.getWithHeader(
+      url: '$URL/api/v1/users/abc/posts/$postId/isLiked',
+      header: {'access-token': token},
+    );
+    return IsLikeModel.fromJson(response.data);
+  }
+
+  Future<IsLikeModel> unLike(
+      {required String postId, required String userId}) async {
+    final response = await DioHelper.deleteWithHeader(
+      url: '$URL/api/v1/users/$userId/posts/$postId/like',
+      header: {'access-token': token},
+    );
+    return IsLikeModel.fromJson(response.data);
   }
 }
