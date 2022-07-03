4414280cf81251e42e9a02732783b6ecefe32410
mustafa-mahmoud203 <must.afa000262@gmail.com>
2022-07-03 17:37:20 +0200

    create day_details_container

--- lib/views/trip/widget/day_details_container.dart
+++ lib/views/trip/widget/day_details_container.dart
@@ -1,8 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 
-class DayDetailsContainer extends StatelessWidget {
-  const DayDetailsContainer(
+class DayTirpDetailsContainer extends StatelessWidget {
+  const DayTirpDetailsContainer(
       {Key? key,
       required this.numOfDAy,
       required this.nameOfCity,
